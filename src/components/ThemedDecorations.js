4c3b8b2d02d73e40009b03404bfe2216d8ce861c
github username <github email address>
2018-03-04 16:05:07 -0500

    Done.

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,24 @@
 // Code ThemedDecoration Component Here
+// 2. This component renders out all of its children, while adding a `className` prop to every child.
+import React from 'react'
+
+export default class ThemedDecorations extends React.Component{
+  // const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+  //    return React.cloneElement(child, {
+  //      isPlaying: child.props.title === currentPlayingTitle
+  //    });
+
+
+  render(){
+    const addClassName = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      })
+    })
+    return(
+      <div>
+        {addClassName}
+      </div>
+    )
+  }
+}
